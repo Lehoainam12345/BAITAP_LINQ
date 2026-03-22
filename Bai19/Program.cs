35adafa8c553153c5d10a67e84dd71045a40d868
Lehoainam12345 <lehoainam21112006@gmail.com>
2026-03-22 08:02:14 +0700

    làm bài 19

---
+++ Bai19/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ma sinh vien : 2415053122327/n Name:Lê Hoài Nam");
+        Console.WriteLine("Class : 24T3");
+        List<int> numbers = new List<int> { 1, 2, 2, 3, 3, 3, 4, 4 };
+
+        var mostFrequent = numbers
+            .GroupBy(n => n)
+            .OrderByDescending(g => g.Count())
+            .First()
+            .Key;
+
+        Console.WriteLine("So xuat hien nhieu nhat: " + mostFrequent);
+        Console.ReadLine();
+    }
+}
